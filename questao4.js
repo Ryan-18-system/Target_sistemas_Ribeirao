9ae5e4ca3c9ed816de5584ee6af5cdba5252c161
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2022-06-25 16:46:53 -0300

    adicionando resolução da questão 4

---
+++ questao4.js
@@ -0,0 +1,5 @@
+/* 
+Imaginamos que duas partículas se encontram  em um determinado trecho do trajeto, a distância que eles estarão no instante
+que eles se cruzam é a mesma, o que ocorre no exemplo é que mesmo para cidade de Franca e de Ribeirão Preto a distância é igual.
+
+*/
