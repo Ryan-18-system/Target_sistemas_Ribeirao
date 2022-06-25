434fb42a689bf63a6aed77eee4c8bef7602913cb
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2022-06-24 23:41:28 -0300

    adicionando comentários explicando a lógica

--- questao3.js
+++ questao3.js
@@ -1,8 +1,8 @@
 /*
-a) 1,3,5,9
-b) 2,4,8,16,32,64,128
-c)0,1,4,9,16,25,36
-d)4,16,36,64,100
-e)1,1,2,3,5,8,13
-f)2,10,12,16,17,18,19,200
+a) 1,3,5,9   Cada número é igual ao anterior + 2, formando os números ímpares
+b) 2,4,8,16,32,64,128 Cada número é igual ao anterior multiplicado por 2.
+c)0,1,4,9,16,25,36  Cada número é igual ao anterior acrescido de um número ímpar que segue a sequência 1, 3, 5, 7, 9, 11, 13...
+d)4,16,36,64,100   Cada número é igual ao quadrado dos números pares
+e)1,1,2,3,5,8,13    Cada número é igual à soma do número atual com o número anterior.
+f)2,10,12,16,17,18,19,200  Sequência formada através de todos os números que iniciam com a letra d. 
 */ 
